3de75c0a3664936705c0196b386ceb7bf4c9a07d
dogiaquocviet <dogiaquocviet@gmail.com>
2022-12-28 16:38:52 +0700

    Testing Exceptions

--- testRunner.cpp
+++ testRunner.cpp
@@ -7,6 +7,25 @@ TEST(TestSample2, TestMulti)
     ASSERT_EQ(multi(2,2), multi2(2,2));
     ASSERT_LT(add(2,3),multi(2,3));
 }
+TEST(ExceptionTest, ANY_THROW)
+{
+    int a = 0;
+    int b = 5;
+    ASSERT_ANY_THROW(dive(b,a));
+}
+TEST(ExceptionTest, THROW)
+{
+    int a = 0;
+    int b = 5;
+    ASSERT_THROW(dive(b,a), std::runtime_error);
+}
+TEST(ExceptionTest, NO_THROW)
+{
+    int a = 1;
+    int b = 5;
+    ASSERT_NO_THROW(dive(b,a));
+}
+
 
 int main(int argc, char **argv)
 {
